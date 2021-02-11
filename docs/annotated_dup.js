50f27914ddaea32a53537cb84b390a38c725d451
scarlens250 <74828654+scarlens250@users.noreply.github.com>
2021-02-11 17:19:52 +0300

    Add files via upload

---
+++ docs/annotated_dup.js
@@ -0,0 +1,11 @@
+var annotated_dup =
+[
+    [ "domain", "namespacedomain.html", [
+      [ "animal", "classdomain_1_1animal.html", "classdomain_1_1animal" ],
+      [ "Cat", "classdomain_1_1_cat.html", "classdomain_1_1_cat" ],
+      [ "Predator", "classdomain_1_1_predator.html", "classdomain_1_1_predator" ]
+    ] ],
+    [ "test", "namespacetest.html", [
+      [ "TestAnimal", "classtest_1_1_test_animal.html", null ]
+    ] ]
+];
---
+++ docs/dir_15c8a2a57156728ba6992d9c9007c840.js
@@ -0,0 +1,4 @@
+var dir_15c8a2a57156728ba6992d9c9007c840 =
+[
+    [ "34---static-methods-scarlens250-1", "dir_dc07731d69aa2a4b921170bc00926f3b.html", "dir_dc07731d69aa2a4b921170bc00926f3b" ]
+];
